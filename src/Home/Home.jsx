3ae34cf2a7174a6bb98c17ced871bd1b970a85a0
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-04 16:14:58 -0500

    Se realiza examen

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.css"
-
+import basedatos from "../utils/basedatos.json"
 export function Home(){
 
     //logica para leer la base de datos
@@ -25,6 +25,7 @@ export function Home(){
                     <p>Bienvenidos a The barber's masters, el lugar donde la tradición y la innovación se encuentran para ofrecerte una experiencia de corte y cuidado personal sin igual. En nuestro elegante espacio, diseñado para el confort y la relajación de nuestros clientes, nos dedicamos a revivir el arte del barbero con un toque contemporáneo. Cada visita a nuestra barbería es más que un simple corte de cabello; es una experiencia holística donde cada detalle está pensado para tu bienestar. Desde el momento en que entras, te sumergirás en un ambiente acogedor, acompañado de un servicio personalizado que atiende tus necesidades específicas. Utilizamos solo productos de la más alta calidad, seleccionados meticulosamente para garantizar resultados excepcionales. Ya sea que busques un corte clásico, un afeitado tradicional, o las últimas tendencias en estilismo, nuestro equipo de profesionales altamente calificados está aquí para superar tus expectativas. En The barber's masters, no solo transformamos tu look, sino que te ofrecemos un momento de pausa y placer en tu día a día. Visítanos y descubre por qué somos más que una barbería; somos una comunidad, un estilo de vida</p>
                    
 
+
                 </div>
                 
             </div>
@@ -32,9 +33,20 @@ export function Home(){
 
        
         <section className="container">
-            <section className="row-cols-1 row-cols-md-5 g-3">
-                {
+            <section className="row row-cols-1 row-cols-md-5 g-3">
+            {
                     //logica para recorrer la BD y mostrar la informacion de los 10 barberos
+                    basedatos.map(function(cliente){
+                        return(
+                            <div className="col">
+                                <div className="card h-100 shadow px-2">
+                                    <h3>{cliente.nombre}</h3>
+                                    <h4>{cliente.especialidad}</h4>
+                                    <h4>{cliente.experiencia}</h4>
+                                </div>
+                            </div>
+                        )
+                    })
                 }
             </section>
         </section>
